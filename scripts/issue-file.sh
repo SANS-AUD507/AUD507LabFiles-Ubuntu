36697eedc86e56145cee93f39e30cd99d7de2e4c
AuditClay <42394776+AuditClay@users.noreply.github.com>
2023-05-25 15:04:07 -0500

    Update issue-file.sh

---
+++ scripts/issue-file.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+cat << EOF > /etc/issue
+####################################################################
+This VM hosts some of the services for the AUD507 course.
+There is no need to log on at the console; access is available via
+SSH from the Windows 10 student VM.
+####################################################################
+
+EOF
+
+IP=$(ip a show ens38 | awk '/inet[^6]/ {print $2}' | grep -v "^10\.50" | sed -e "s/\/24//")
+
+echo "\n####################################################################" >> /etc/issue
+echo "Host only IP is $IP"
+echo "EWB website is available at http://$IP:507/workbook"
+echo "####################################################################\n\n" >> /etc/issue
