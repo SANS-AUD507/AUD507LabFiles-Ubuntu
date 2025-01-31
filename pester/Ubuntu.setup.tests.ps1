2041e1b9081d88e97ca57b38f77acecfc2713590
AuditClay <42394776+AuditClay@users.noreply.github.com>
2025-01-31 13:46:13 -0600

    pester test for azure account

--- pester/Ubuntu.setup.tests.ps1
+++ pester/Ubuntu.setup.tests.ps1
@@ -8,283 +8,283 @@ $config.Run.Path='/home/student/AUD507-Labs/pester/Ubuntu.Setup.tests.ps1'
 Invoke-Pester -Configuration $config
 #>
 Describe 'Lab Setup tests for 507Ubuntu VM' {
-  BeforeDiscovery {
-    #If the AWS config files are not there, then skip the AWS tests
-    if( -not ( (Test-Path -Type Leaf -Path /home/student/.aws/credentials) -or (Test-Path -Type Leaf -Path /home/student/.aws/config) ) ) {
-      Write-Host "Skipping AWS tests because config files do not exist"
-      $skipAWS = $true
-    }
-    else {
-      #Skip the Cloud Services tests if there are no good AWS credentials
-      $userARN = (aws sts get-caller-identity | jq '.Arn')
-      if( $userARN -notlike '*student*'){
-        Write-Host "Skipping AWS tests because config files do not exist"
-        $skipAWS = $true
-      }
-    }
+    BeforeDiscovery {
+        #If the AWS config files are not there, then skip the AWS tests
+        if ( -not ( (Test-Path -Type Leaf -Path /home/student/.aws/credentials) -or (Test-Path -Type Leaf -Path /home/student/.aws/config) ) ) {
+            Write-Host "Skipping AWS tests because config files do not exist"
+            $skipAWS = $true
+        }
+        else {
+            #Skip the Cloud Services tests if there are no good AWS credentials
+            $userARN = (aws sts get-caller-identity | jq '.Arn')
+            if ( $userARN -notlike '*student*') {
+                Write-Host "Skipping AWS tests because config files do not exist"
+                $skipAWS = $true
+            }
+        }
 
-    #If the Azure configuration is not there, then skip the Azure tests
-    $azSubCount = (Get-Content /home/student/.azure/azureProfile.json | ConvertFrom-Json).Subscriptions.Count
-    if( $azSubCount -lt 1) {
-      Write-Host "Skipping Azure tests because config files do not exist"
-      $skipAzure = $true
-    } 
-  }
+        #If the Azure configuration is not there, then skip the Azure tests
+        $azSubCount = (Get-Content /home/student/.azure/azureProfile.json | ConvertFrom-Json).Subscriptions.Count
+        if ( $azSubCount -lt 1) {
+            Write-Host "Skipping Azure tests because config files do not exist"
+            $skipAzure = $true
+        } 
+    }
 
   
-  #Check basic network setup to ensure local and internet connectivity
-  Context 'Network connectivity' {
-    It 'Ping 507Win10 - HostOnly' {
-        $arpRes = (sudo arping -c 1 10.50.7.101 | awk '/transmitted/ { print $4 }')
-        $arpRes | Should -BeExactly 1 -Because 'Ensure that first network adapter is set to HostOnly'
+    #Check basic network setup to ensure local and internet connectivity
+    Context 'Network connectivity' {
+        It 'Ping 507Win10 - HostOnly' {
+            $arpRes = (sudo arping -c 1 10.50.7.101 | awk '/transmitted/ { print $4 }')
+            $arpRes | Should -BeExactly 1 -Because 'Ensure that first network adapter is set to HostOnly'
+        }
+
+        It 'Ping Google - NAT' {
+            $pingRes = (ping -c 4 dns.google | awk '/transmitted/ { print $4 }')
+            $pingRes | Should -BeGreaterThan 0 -Because 'Ensure that second network adapter is set to NAT '
+        }
     }
 
-    It 'Ping Google - NAT' {
-        $pingRes = (ping -c 4 dns.google | awk '/transmitted/ { print $4 }')
-        $pingRes | Should -BeGreaterThan 0 -Because 'Ensure that second network adapter is set to NAT '
-    }
-  }
+    #Required ports are open
+    Context 'Local TCP ports' {
+        #Get a list of listening ports to use in all tests
+        BeforeAll {
+            $localPorts = (sudo netstat -antp | awk '/LISTEN/ { print $4 }')
+        }
 
-  #Required ports are open
-  Context 'Local TCP ports' {
-    #Get a list of listening ports to use in all tests
-    BeforeAll {
-        $localPorts = (sudo netstat -antp | awk '/LISTEN/ { print $4 }')
-    }
+        It 'Graphite on port 2003' {
+            $localPorts | Should -Contain '0.0.0.0:2003' 
+        }
 
-    It 'Graphite on port 2003' {
-        $localPorts | Should -Contain '0.0.0.0:2003' 
-    }
+        It 'Grafana on port 3000' {
+            $localPorts | Should -Contain ':::3000' 
+        }
 
-    It 'Grafana on port 3000' {
-        $localPorts | Should -Contain ':::3000' 
-    }
+        It 'Default Nginx site HTTP/HTTPS' {
+            $localPorts | Should -Contain '10.50.7.50:80'
+            $localPorts | Should -Contain '10.50.7.50:443'
+        }
 
-    It 'Default Nginx site HTTP/HTTPS' {
-        $localPorts | Should -Contain '10.50.7.50:80'
-        $localPorts | Should -Contain '10.50.7.50:443'
-    }
+        It 'Nginx BWApp' {
+            $localPorts | Should -Contain '10.50.7.22:80'
+        }
 
-    It 'Nginx BWApp' {
-        $localPorts | Should -Contain '10.50.7.22:80'
-    }
+        It 'Nginx DVWA' {
+            $localPorts | Should -Contain '10.50.7.24:80'
+        }
 
-    It 'Nginx DVWA' {
-        $localPorts | Should -Contain '10.50.7.24:80'
-    }
+        It 'Nginx WackPicko' {
+            $localPorts | Should -Contain '10.50.7.23:80'
+        }
 
-    It 'Nginx WackPicko' {
-        $localPorts | Should -Contain '10.50.7.23:80'
-    }
+        It 'Nginx Juice Shop HTTP/HTTPS' {
+            $localPorts | Should -Contain '10.50.7.20:80'
+            $localPorts | Should -Contain '10.50.7.20:443'
+        }
 
-    It 'Nginx Juice Shop HTTP/HTTPS' {
-        $localPorts | Should -Contain '10.50.7.20:80'
-        $localPorts | Should -Contain '10.50.7.20:443'
-    }
+        It 'Nessus' {
+            $localPorts | Should -Contain '10.50.7.29:8834'
+        }
 
-    It 'Nessus' {
-        $localPorts | Should -Contain '10.50.7.29:8834'
-    }
-
-    It 'Workbook' {
-        $localPorts | Should -Contain '0.0.0.0:507'
-    }
-  }
-
-  #Look for appropriate content on class websites
-  Context 'Websites' {
-    It 'Grafana login form' {
-        #look for "Grafana" in the login page
-        $res = (curl -s http://localhost:3000/login | grep -ci 'grafana')
-        $res | Should -BeGreaterThan 0
+        It 'Workbook' {
+            $localPorts | Should -Contain '0.0.0.0:507'
+        }
     }
 
-    It 'Default website has workbook link' {
-        $res = (curl -s http://10.50.7.50:80 | grep -ci workbook)
-        $res | Should -BeExactly 1  
-    }
+    #Look for appropriate content on class websites
+    Context 'Websites' {
+        It 'Grafana login form' {
+            #look for "Grafana" in the login page
+            $res = (curl -s http://localhost:3000/login | grep -ci 'grafana')
+            $res | Should -BeGreaterThan 0
+        }
 
-    It 'Workbook website has lab links' {
-        $res = (curl -s http://10.50.7.50:80/workbook/ | grep -ci " lab [0-9]\.[0-9]")
-        $res | Should -BeGreaterThan 10
-    }
+        It 'Default website has workbook link' {
+            $res = (curl -s http://10.50.7.50:80 | grep -ci workbook)
+            $res | Should -BeExactly 1  
+        }
 
-    It 'BWapp database install' {
-        $res = (curl -s  http://10.50.7.22/install.php?install=yes | grep -ci bwapp)
-        $res | Should -BeGreaterThan 0
-    }
+        It 'Workbook website has lab links' {
+            $res = (curl -s http://10.50.7.50:80/workbook/ | grep -ci " lab [0-9]\.[0-9]")
+            $res | Should -BeGreaterThan 10
+        }
 
-    It 'DVWA login page' {
-        $res = (curl -s http://10.50.7.24:80/login.php | grep -ci dvwa)
-        $res | Should -BeGreaterThan 0
-    }
+        It 'BWapp database install' {
+            $res = (curl -s  http://10.50.7.22/install.php?install=yes | grep -ci bwapp)
+            $res | Should -BeGreaterThan 0
+        }
+
+        It 'DVWA login page' {
+            $res = (curl -s http://10.50.7.24:80/login.php | grep -ci dvwa)
+            $res | Should -BeGreaterThan 0
+        }
     
-    It 'WackoPicko front page' {
-        $res = (curl -s http://10.50.7.23:80 | grep -ci wackopicko)
-        $res | Should -BeGreaterThan 0
+        It 'WackoPicko front page' {
+            $res = (curl -s http://10.50.7.23:80 | grep -ci wackopicko)
+            $res | Should -BeGreaterThan 0
+        }
+
+        It 'Juice Shop front page HTTP' {
+            $res = (curl -s http://10.50.7.20:80 | grep -ci juice)
+            $res | Should -BeGreaterThan 0
+        }
+
+        It 'Juice Shop front page HTTPS' {
+            $res = (curl -s -k https://10.50.7.20:443 | grep -ci juice)
+            $res | Should -BeGreaterThan 0
+        }
+
+        It 'Nessus startup page' {
+            $res = (curl -s -k https://10.50.7.29:8834 | grep -ci nessus)
+            $res | Should -BeGreaterThan 0
+        }
+
+        It 'Workbook home page' {
+            $res = (curl -s http://10.50.7.20:507/workbook/ | grep -ci aud507)
+            $res | Should -BeGreaterOrEqual 1
+        }
     }
 
-    It 'Juice Shop front page HTTP' {
-        $res = (curl -s http://10.50.7.20:80 | grep -ci juice)
-        $res | Should -BeGreaterThan 0
-    }
-
-    It 'Juice Shop front page HTTPS' {
-        $res = (curl -s -k https://10.50.7.20:443 | grep -ci juice)
-        $res | Should -BeGreaterThan 0
-    }
-
-    It 'Nessus startup page' {
-        $res = (curl -s -k https://10.50.7.29:8834 | grep -ci nessus)
-        $res | Should -BeGreaterThan 0
-    }
-
-    It 'Workbook home page' {
-        $res = (curl -s http://10.50.7.20:507/workbook/ | grep -ci aud507)
-        $res | Should -BeGreaterOrEqual 1
-    }
-  }
-
-  #Ensure systemd services are running
-  Context 'Systemd services' {
+    #Ensure systemd services are running
+    Context 'Systemd services' {
     
-    It 'Carbon cache' {
-        $res = (systemctl --no-pager status carbon-cache.service | grep -ci "active (running)")
-        $res | should -BeExactly 1
+        It 'Carbon cache' {
+            $res = (systemctl --no-pager status carbon-cache.service | grep -ci "active (running)")
+            $res | Should -BeExactly 1
+        }
+
+        It 'Graphite-api' {
+            $res = (systemctl --no-pager status graphite-api.service | grep -ci "active (running)")
+            $res | Should -BeExactly 1
+        }
+
+        It 'Nginx' {
+            $res = (systemctl --no-pager status nginx.service | grep -ci "active (running)")
+            $res | Should -BeExactly 1
+        }
+
+        It 'Microk8s kubelite' {
+            $res = (systemctl --no-pager status snap.microk8s.daemon-kubelite.service | grep -ci "active (running)")
+            $res | Should -BeExactly 1
+        }
+
+        It 'Grafana' {
+            $res = (systemctl --no-pager status  grafana-server.service | grep -ci "active (running)")
+            $res | Should -BeExactly 1
+        }
     }
 
-    It 'Graphite-api' {
-        $res = (systemctl --no-pager status graphite-api.service | grep -ci "active (running)")
-        $res | should -BeExactly 1
+    #Check k8s services for appropriate published TCP ports
+    Context 'Kubernetes TCP Ports' {
+        BeforeAll {
+            $k8sServices = (microk8s kubectl get services | awk '/NodePort/ { print $5 }')
+        }
+
+        It 'juice-shop' {
+            $k8sServices | Should -Contain '8000:30020/TCP'
+        }
+
+        It 'dvwa' {
+            $k8sServices | Should -Contain '8000:30024/TCP'
+        }
+
+        It 'bwapp' {
+            $k8sServices | Should -Contain '8000:30022/TCP'
+        }
+
+        It 'wackopicko' {
+            $k8sServices | Should -Contain '8000:30023/TCP'
+        }
     }
 
-    It 'Nginx' {
-        $res = (systemctl --no-pager status nginx.service | grep -ci "active (running)")
-        $res | should -BeExactly 1
+    Context 'Kubernetes Pod Status' {
+        It 'juice-shop' {
+            $state = (microk8s kubectl get pods | awk '/juice-shop/ {print $3}')
+            $state | Should -Be 'Running'
+        }
+
+        It 'dvwa' {
+            $state = (microk8s kubectl get pods | awk '/dvwa/ {print $3}')
+            $state | Should -Be 'Running'
+        }
+
+        It 'bwapp' {
+            $state = (microk8s kubectl get pods | awk '/bwapp/ {print $3}')
+            $state | Should -Be 'Running'
+        }
+
+        It 'wackopicko' {
+            $state = (microk8s kubectl get pods | awk '/wackopicko/ {print $3}')
+            $state | Should -Be 'Running'
+        }
     }
 
-    It 'Microk8s kubelite' {
-        $res = (systemctl --no-pager status snap.microk8s.daemon-kubelite.service | grep -ci "active (running)")
-        $res | should -BeExactly 1
+    #Local system checks
+    Context 'Local system' {
+        It 'Disk Used < 75%' {
+            $usedPct = [int32](df -h | awk '/ \/$/ { print $5 }' | sed -e 's/%//')
+            $usedPct | Should -BeLessThan 75
+        }
     }
 
-    It 'Grafana' {
-        $res = (systemctl --no-pager status  grafana-server.service | grep -ci "active (running)")
-        $res | should -BeExactly 1
-    }
-  }
+    #Check that cloud credenitals/configs can return results
+    Context 'Cloud CLI configuration - AWS' -Skip:$skipAWS {
+        It 'AWS credentials are working' {
+            $arn = aws sts get-caller-identity | awk '/Arn/ {print $2}'
+            $arn | Should -BeLike '*arn*'
+        }
 
-  #Check k8s services for appropriate published TCP ports
-  Context 'Kubernetes TCP Ports' {
-    BeforeAll {
-        $k8sServices = (microk8s kubectl get services | awk '/NodePort/ { print $5 }')
-    }
+        It 'AWS config is set to us-east-2 region' {
+            '/home/student/.aws/config' | Should -FileContentMatch 'region = us-east-2'
+        }
 
-    It 'juice-shop' {
-        $k8sServices | Should -Contain '8000:30020/TCP'
-    }
+        It 'AWS config is set to json output' {
+            '/home/student/.aws/config' | Should -FileContentMatch 'output = json'
+        }
+    } 
 
-    It 'dvwa' {
-        $k8sServices | Should -Contain '8000:30024/TCP'
-    }
+    # TODO: Move to lab tests :)
+    Context 'Config files for labs' {
+        It 'Custodian IAM yaml file exists' {
+            '/home/student/AUD507-Labs/custodian/aws_iam.yaml' | Should -Exist
+            '/home/student/AUD507-Labs/custodian/aws_iam.yaml' | Should -FileContentMatch 'iam-no-mfa'
+            '/home/student/AUD507-Labs/custodian/aws_iam.yaml' | Should -FileContentMatch 'iam-inline-policy'
+        }
+        It 'Custodian Ingress yaml file exists' {
+            '/home/student/AUD507-Labs/custodian/aws_ingress.yaml' | Should -Exist
+            '/home/student/AUD507-Labs/custodian/aws_ingress.yaml' | 
+                Should -FileContentMatch 'aws-ingress-admin-ports-allowed'
+        }
 
-    It 'bwapp' {
-        $k8sServices | Should -Contain '8000:30022/TCP'
-    }
+        It 'Terraform main.tf file exists' {
+            '/home/student/AUD507-Labs/infrastructure/terraform/aws/modules/storage/main.tf' | Should -Exist
+            '/home/student/AUD507-Labs/infrastructure/terraform/aws/modules/storage/main.tf' |             
+                Should -FileContentMatch 'aws_db_instance'
+        }
 
-    It 'wackopicko' {
-        $k8sServices | Should -Contain '8000:30023/TCP'
-    }
-  }
+        It 'Cloudquery.io aws.yml file exists' {
+            '/home/student/AUD507-Labs/cloudquery.io/config/aws.yml'
+            '/home/student/AUD507-Labs/cloudquery.io/config/aws.yml' |             
+                Should -FileContentMatch 'cloudquery/aws'
+        }
 
-  Context 'Kubernetes Pod Status' {
-    It 'juice-shop' {
-        $state = (microk8s kubectl get pods | awk '/juice-shop/ {print $3}')
-        $state | Should -Be 'Running'
-    }
+        It 'Cloudquery.io azure.yml file exists' {
+            '/home/student/AUD507-Labs/cloudquery.io/config/azure.yml'
+            '/home/student/AUD507-Labs/cloudquery.io/config/azure.yml' |             
+                Should -FileContentMatch 'cloudquery/azure'
+        }
 
-    It 'dvwa' {
-        $state = (microk8s kubectl get pods | awk '/dvwa/ {print $3}')
-        $state | Should -Be 'Running'
-    }
-
-    It 'bwapp' {
-        $state = (microk8s kubectl get pods | awk '/bwapp/ {print $3}')
-        $state | Should -Be 'Running'
-    }
-
-    It 'wackopicko' {
-        $state = (microk8s kubectl get pods | awk '/wackopicko/ {print $3}')
-        $state | Should -Be 'Running'
-    }
-  }
-
-  #Local system checks
-  Context 'Local system' {
-    It 'Disk Used < 75%' {
-        $usedPct = [int32](df -h | awk '/ \/$/ { print $5 }' | sed -e 's/%//')
-        $usedPct | should -BeLessThan 75
-    }
-  }
-
-  #Check that cloud credenitals/configs can return results
-  Context 'Cloud CLI configuration - AWS' -skip:$skipAWS {
-    It 'AWS credentials are working' {
-      $arn = aws sts get-caller-identity | awk '/Arn/ {print $2}'
-      $arn | Should -BeLike '*arn*'
+        It 'Cloudquery.io postgresql.yml file exists' {
+            '/home/student/AUD507-Labs/cloudquery.io/config/postgresql.yml'
+            '/home/student/AUD507-Labs/cloudquery.io/config/postgresql.yml' |             
+                Should -FileContentMatch 'cloudquery/postgresql'
+        }
     }
 
-    It 'AWS config is set to us-east-2 region' {
-    '/home/student/.aws/config' | should -FileContentMatch 'region = us-east-2'
+    Context 'Cloud CLI configuration - Azure' -Skip:$skipAzure {
+        It 'Azure credentials are working' {
+            $username = (az account show | jq '.user.name')
+            $username | Should -BeLike '"student*@*'
+        }
     }
-
-    It 'AWS config is set to json output' {
-    '/home/student/.aws/config' | should -FileContentMatch 'output = json'
-    }
-  } 
-
-  # TODO: Move to lab tests :)
-  Context 'Config files for labs' {
-    It 'Custodian IAM yaml file exists' {
-        '/home/student/AUD507-Labs/custodian/aws_iam.yaml' | should -Exist
-        '/home/student/AUD507-Labs/custodian/aws_iam.yaml' | should -FileContentMatch 'iam-no-mfa'
-        '/home/student/AUD507-Labs/custodian/aws_iam.yaml' | should -FileContentMatch 'iam-inline-policy'
-    }
-    It 'Custodian Ingress yaml file exists' {
-        '/home/student/AUD507-Labs/custodian/aws_ingress.yaml' | should -Exist
-        '/home/student/AUD507-Labs/custodian/aws_ingress.yaml' | 
-        should -FileContentMatch 'aws-ingress-admin-ports-allowed'
-    }
-
-    It 'Terraform main.tf file exists' {
-        '/home/student/AUD507-Labs/infrastructure/terraform/aws/modules/storage/main.tf' | should -Exist
-        '/home/student/AUD507-Labs/infrastructure/terraform/aws/modules/storage/main.tf' |             
-        should -FileContentMatch 'aws_db_instance'
-    }
-
-    It 'Cloudquery.io aws.yml file exists' {
-        '/home/student/AUD507-Labs/cloudquery.io/config/aws.yml'
-        '/home/student/AUD507-Labs/cloudquery.io/config/aws.yml' |             
-        should -FileContentMatch 'cloudquery/aws'
-    }
-
-    It 'Cloudquery.io azure.yml file exists' {
-        '/home/student/AUD507-Labs/cloudquery.io/config/azure.yml'
-        '/home/student/AUD507-Labs/cloudquery.io/config/azure.yml' |             
-        should -FileContentMatch 'cloudquery/azure'
-    }
-
-    It 'Cloudquery.io postgresql.yml file exists' {
-        '/home/student/AUD507-Labs/cloudquery.io/config/postgresql.yml'
-        '/home/student/AUD507-Labs/cloudquery.io/config/postgresql.yml' |             
-        should -FileContentMatch 'cloudquery/postgresql'
-    }
-  }
-
-  Context 'Cloud CLI configuration - Azure' -skip:$skipAzure {
-    It 'Azure credentials are working' {
-        $username = (az account show | jq '.user.name')
-        $username | Should -BeLike '"student@*'
-    }
-  }
 }
